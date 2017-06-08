29cc8d19b1871b3ffa1aab01909b1a8e1941cbfa
Adam <adamho.bhc@gmail.com>
2017-06-08 14:41:28 +0800

    Second Commit

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -5,5 +5,6 @@
 class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
+        System.out.println("Changes after First commit!");
     }
 }
