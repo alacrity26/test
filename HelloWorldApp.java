0d41576e7370c9ef05b90706105c0c55969e1357
Adam <adamho.bhc@gmail.com>
2017-06-08 14:53:13 +0800

    Third Commit

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -6,5 +6,6 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
         System.out.println("Changes after First commit!");
+        System.out.println("Changes after Pushing to remote!");
     }
 }
